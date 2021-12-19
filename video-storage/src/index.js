04dbff9515538a2f025a5b619b6f268fb58b9738
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-18 23:27:51 -0600

    Add apikey to CONFIG.

--- video-storage/src/index.js
+++ video-storage/src/index.js
@@ -32,7 +32,7 @@ const CONFIG = {
     accessKeyId: HMAC_ACCESS_KEY_ID,
     secretAccessKey: HMAC_SECRET_ACCESS_KEY
   },
-//  endpoint: ENDPOINT,
+  endpoint: ENDPOINT,
 //  apiKeyId: API_KEY,
   //serviceInstanceId: SERVICE_INSTANCE_ID,
   //ibmAuthEndpoint: "https://iam.cloud.ibm.com/identity/token",
