fb9f5d4bd8651df5441699e66c4e315582cd48f7
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-18 23:12:11 -0600

    Test CONFIG structure.

--- video-storage/src/index.js
+++ video-storage/src/index.js
@@ -28,10 +28,12 @@ const REGION = process.env.REGION;
 const PORT = process.env.PORT && parseInt(process.env.PORT) || 3000;
 let READINESS_PROBE = false;
 const CONFIG = {
-  endpoint: ENDPOINT,
-  apiKeyId: API_KEY,
-  accessKeyId: HMAC_ACCESS_KEY_ID,
-  secretAccessKey: HMAC_SECRET_ACCESS_KEY,
+  credentials: {
+    accessKeyId: HMAC_ACCESS_KEY_ID,
+    secretAccessKey: HMAC_SECRET_ACCESS_KEY
+  },
+//  endpoint: ENDPOINT,
+//  apiKeyId: API_KEY,
   //serviceInstanceId: SERVICE_INSTANCE_ID,
   //ibmAuthEndpoint: "https://iam.cloud.ibm.com/identity/token",
   region: REGION
